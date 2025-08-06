224f19c16f55f20481f22dc2385b25724af480af
ShadowVonGreywolf <deni.andrei13@gmail.com>
2025-08-06 20:46:53 +0300

    learning angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <app-header></app-header>
     <router-outlet />
   `,
-  styles: [],
+  styles: [
+    `
+      h1 {
+        padding: 5px;
+        
+      }
+    `
+  ],
 })
 export class AppComponent {
   title = 'SadenGamesMedia';
